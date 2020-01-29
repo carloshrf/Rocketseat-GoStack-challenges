4677a8f20d131feae3c2ce58601c4c8bc78a58ec
Carloshrf <carlos_6282010@hotmail.com>
2020-01-29 13:19:21 -0300

    Desafio 1 completo

---
+++ Desafio01/index.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const server = express();
+
+server.use(express.json());
+
+const projects = []; // Receberá os projetos cadastrados
+var request = 0;     // Receberá a quantidade de requisições por meio de um middleware
+
+//================================
+// <MIDDLEWARES>
+//================================
+// Check if a project exists by ID
+function checkProjectExists(req, res, next){
+  const project = projects.find(project => project.id == req.params.id);
+  if (!project){
+    return res.status(400).json('Project does not exists');
+  }
+
+  return next();
+}
+
+// Return the number of requests to the API
+function registerRequest(req, res, next) {
+  console.log('Requests: '+ ++request);
+  return next();
+}
+
+//================================
+// </MIDDLEWARES>
+//================================
+
+// Creates a project
+server.post('/projects', registerRequest, (req, res) => {
+  const data = {
+    id: req.body.id,
+    title: req.body.title,
+    tasks: []
+  }
+  projects.push(data);
+  
+  return res.send(`Project was successfully added`);
+})
+
+// Lists all projects
+server.get('/projects', registerRequest, (req, res) => {
+  return res.json(projects);
+})
+
+// Change any project
+server.put('/projects/:id', registerRequest, checkProjectExists, (req, res) => {
+  const project = projects.find(project => project.id == req.params.id);
+  project.title = req.body.title;
+  return res.json(project);
+})
+
+// Delete project
+server.delete('/projects/:id', registerRequest, checkProjectExists, (req, res) => {
+  const projectIndex = projects.findIndex(project => project.id == req.params.id);
+  projects.splice(projectIndex, 1);
+  return res.send('Projeto removido');
+})
+
+// Add tasks to projects
+server.post('/projects/:id/tasks', registerRequest, checkProjectExists, (req, res) => {
+  const project = projects.find(project => project.id == req.params.id);
+  project.tasks.push(req.body.title);
+  return res.send('Tasks added!');
+})
+
+server.listen(3000);
